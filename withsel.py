5524cd80dc96088fea37f4139861d32770d5def5
AgustinBustos <agustinbustosbarton@gmail.com>
2024-06-24 11:38:47 -0300

    g

--- withsel.py
+++ withsel.py
@@ -11,7 +11,8 @@ if __name__=='__main__':
     df=pd.read_excel('Report complete MFA 6.3.24 ALM UPC w attributes ES Non-Numeric PC ONLY.xlsx',sheet_name='Sales by UPC')
     all_prods=list(df['Product'].unique())
     all_links=[f"https://www.walmart.com/search?q={i.split(' - ')[0].replace(' ','+')}" for i in all_prods]
-    connections={}
+    connections=[]
+    urls=[]
     # # print(all_links)
     # url=all_links[0]
     # url='https://www.walmart.com/search?q=LAYS+POTATO+CHIP+CLASSIC+BAG+8+OZ'
@@ -23,27 +24,35 @@ if __name__=='__main__':
     # time.sleep(10000)
     print(len(all_links))
     for url in all_links:
-        print(url)
-        driver.get(url)
-        element = driver.find_element(By.CSS_SELECTOR, "div[data-testid='item-stack']")
-        # element.find_element(By.CSS_SELECTOR, ">:nth-child(1)").click()
-        allprods=element.find_elements(By.XPATH, "./*")
-        allprods[0].click()
-        time.sleep(5)
-        
-        images = driver.find_elements(By.CSS_SELECTOR, "img")
-        time.sleep(5)
-        print('------------------------------------------------------------')
-        print(images)
-        time.sleep(dt)
-        # print([i.get_attribute('outerHTML') for i in images])
-        # print(element.get_attribute('innerHTML'))
         try:
-            connections[url]=[i.get_attribute('outerHTML') for i in images]
-        except:
-            print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-            connections[url]=[]
-
-        pd.DataFrame(connections).to_csv('scraped.csv')
+            print(url)
+            driver.get(url)
+            element = driver.find_element(By.CSS_SELECTOR, "div[data-testid='item-stack']")
+            # element.find_element(By.CSS_SELECTOR, ">:nth-child(1)").click()
+            allprods=element.find_elements(By.XPATH, "./*")
+            allprods[0].click()
+            time.sleep(5)
+            
+            images = driver.find_elements(By.CSS_SELECTOR, "img")
+            time.sleep(5)
+            print('------------------------------------------------------------')
+            print(images)
+            time.sleep(dt)
+            # print([i.get_attribute('outerHTML') for i in images])
+            # print(element.get_attribute('innerHTML'))
+            try:
+                connections.append("///".join([i.get_attribute('outerHTML') for i in images]))
+                urls.append(url)
+            except:
+                print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+                connections.append("")
+                urls.append(url)
+            pd.DataFrame({"upc":urls,"value":connections}).to_csv('scraped.csv')
+        except Exception as e:
+            print(e)
+            connections.append('none')
+            urls.append(url)
+            pd.DataFrame({"upc":urls,"value":connections}).to_csv('scraped.csv')
+            # time.sleep(100000)
         # time.sleep(10000)
     # print(connections)
